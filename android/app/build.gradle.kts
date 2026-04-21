3ed4ecae425c7a80e1d1019ca7677a67b2d33ebe
priyanshu <priyanshuofficial2004@gmail.com>
2026-04-21 15:00:05 +0530

    fix: resolve release build failure and update CI config

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -20,12 +20,12 @@ android {
     }
 
     defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
+        // Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
         applicationId = "com.example.remote"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
-        targetSdk = flutter.targetSdkVersion
+        targetSdk = 36
         versionCode = flutter.versionCode
         versionName = flutter.versionName
     }
